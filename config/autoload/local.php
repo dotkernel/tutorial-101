cee68d8e2599ba8435cf0342d64a2ab6ba405831
Howriq <horea@rospace.com>
2025-11-23 21:39:05 +0200

    local.php
    Signed-off-by: Howriq <horea@rospace.com>

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -11,35 +11,13 @@ declare(strict_types=1);
 
 $baseUrl = 'http://light.dotkernel.localhost';
 
-$databases = [
-    'default' => [
-        'host'     => 'localhost',
-        'dbname'   => 'light',
-        'user'     => 'root',
-        'password' => '123',
-        'port'     => 3306,
-        'driver'   => 'pdo_mysql',
-        'charset'  => 'utf8mb4',
-        'collate'  => 'utf8mb4_general_ci',
-    ],
-    // you can add more database connections into this array
-];
-
 return [
-    'databases'   => $databases,
-    'doctrine'    => [
-        'connection' => [
-            'orm_default' => [
-                'params' => $databases['default'],
-            ],
-        ],
-    ],
     'application' => [
         'url' => $baseUrl,
     ],
-    'routes'      => [
+    'routes' => [
         'page' => [
-            'about'      => 'about',
+            'about' => 'about',
             'who-we-are' => 'who-we-are',
         ],
     ],
